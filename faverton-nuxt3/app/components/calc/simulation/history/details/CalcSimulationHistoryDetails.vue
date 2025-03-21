02fa8ed526aa257652569047e7b43dd4ba52426a
huor97 <huor97ka@gmail.com>
2025-03-21 16:41:50 +0100

    delete button don't used

--- faverton-nuxt3/app/components/calc/simulation/history/details/CalcSimulationHistoryDetails.vue
+++ faverton-nuxt3/app/components/calc/simulation/history/details/CalcSimulationHistoryDetails.vue
@@ -187,9 +187,6 @@ const simulationDate = computed(() => props.selectedSimulation?.simulation_date 
     </div>
 
     <div class="mt-6 flex justify-end gap-3">
-      <button class="px-4 py-2 bg-gray-200 rounded hover:bg-gray-300">
-        Refaire cette simulation
-      </button>
       <button class="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600">
         Supprimer de l'historique
       </button>
