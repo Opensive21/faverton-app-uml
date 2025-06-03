3f35a2361b161685600048b17d593947ead6440a
Huor97 <hour97ka@gmail.com>
2025-06-04 00:53:47 +0200

    update ci

--- faverton-nuxt3/playwright.config.ts
+++ faverton-nuxt3/playwright.config.ts
@@ -33,8 +33,9 @@ export default defineConfig({
   ],
 
   webServer: {
-    command: `pnpm run dev`,
+    command: process.env.CI ? `pnpm preview` : `pnpm run dev`,
     url: `http://localhost:3000`,
     reuseExistingServer: !process.env.CI,
+    timeout: 120 * 1000,
   },
 });
